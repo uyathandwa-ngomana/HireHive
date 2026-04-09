dfa6a057189594fe4d235780ca0b9cbeb0fc7365
Uyathandwa Ngomana <231173229@mycput.ac.za>
2026-04-09 22:13:39 +0200

    updated HireHive project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { useState } from "react";
 
 function App() { 
-  const [job, setJob] = useState("");    //use state lets me to store data 
+  const [job, setJob] = useState("");  //use state lets me to store data 
+  const [jobs, setJobs] = useState([]);
+  
+  const addJob = () => {
+    setJobs([...jobs, job]);  // keep the old jobd snd also add new one
+    setJob("");
+  };
   return (
     <div>
-      <h1>Job Tracker</h1>
+      <h1>HireHive</h1>
 
       <input
       type="text"
       placeholder="Enter Job"
       value={job}
-      onChange={(e) => setJob(e.target.value)}
+      onChange={(e) => setJob(e.target.value)}  // it updates the jobs when typing
       />
+      <button onClick={addJob}>Add Job</button>
 
+      <ul>
+        {jobs.map((j, index) => (
+          <li key={index}>{j}</li>
+        ))}
+      </ul>
     </div>
   );
 }
