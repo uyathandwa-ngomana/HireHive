75809c23709dc32533f4851d27ddc593e1d0a853
Uyathandwa Ngomana <231173229@mycput.ac.za>
2026-04-09 21:06:35 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function App() { 
+  const [job, setJob] = useState("");    //use state lets me to store data 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Job Tracker</h1>
+
+      <input
+      type="text"
+      placeholder="Enter Job"
+      value={job}
+      onChange={(e) => setJob(e.target.value)}
+      />
+
     </div>
   );
 }
